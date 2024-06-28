b585683996d1709c35981ce90eff0441b94138a9
sonak1029 <142824400+sonak1029@users.noreply.github.com>
2024-06-28 13:08:44 +0000

    container config done

--- 2_Sorting_Algorithm/01_Bubble_Sort.cpp
+++ 2_Sorting_Algorithm/01_Bubble_Sort.cpp
@@ -10,11 +10,12 @@ bool compare(int a, int b){
 
 void bubble_Sort(int *arr, int size){
     
-    for(int i=1; i<size; i++){
+    for(int i=1; i<=size-1; i++){ // i=1 1<=4
         // Repeted Swaping
-        for(int j=0; j<size-i; j++){
-            if(compare(arr[j], arr[j+1])){
-                swap(arr[j], arr[j+1]);
+        // And this loop will go for index n-2
+        for(int j=0; j<=size-1-i; j++){ // j=0 0<=3 1<=3 2<=3 3<=3
+            if(compare(arr[j], arr[j+1])){// 5 > 1
+                swap(arr[j], arr[j+1]); // 4 5 3 2 1 // 4 3 5 2 1 // 4 3 2 5 1 // 4 3 2 1 5
             }
         }
         cout <<endl;
