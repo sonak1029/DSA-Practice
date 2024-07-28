8d5b27c8323a34da164214109d1cfc8b86d6c7a3
sonak1029 <142824400+sonak1029@users.noreply.github.com>
2024-07-28 13:37:21 +0000

    container config done

---
+++ 4_Recursion/01_Intro.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(int n){
+    if(n>0){
+        fun(n-1);
+        cout <<n <<" " <<endl;
+    }
+}
+
+int main(){
+    int x = 3;
+
+    fun(x);
+
+    return 0;
+}
