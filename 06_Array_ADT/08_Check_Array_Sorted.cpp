fff3d302da3f0a64426e7a3f1e46b318a3417f65
sonak1029 <142824400+sonak1029@users.noreply.github.com>
2024-09-19 07:19:20 +0000

    practice sorted Array Problem

---
+++ 06_Array_ADT/08_Check_Array_Sorted.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Array{
+    int A[15];
+    int size;
+    int length;
+};
+
+
+// Display Array
+void Display(struct Array *arr){
+    for(int i=0; i<arr->length; i++){
+        cout <<arr->A[i] <<" ";
+    }cout <<endl;
+}
+
+
+
+// Insert A value in Sorted Array
+void Sorted_Array(struct Array *arr, int value){
+
+    int i = arr->length-1;
+
+    while(arr->A[i] > value){
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+
+    arr->A[i+1] = value;
+    arr->length++;
+
+}
+
+
+
+// Check Whether A Positive Number is Sorted Or Not
+bool Algorithum_is_Sorted(struct Array *arr){
+
+    for(int i=0; i<arr->length-1; i++){
+        if(arr->A[i] > arr->A[i+1]){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+
+
+// Check Whether A Negative Number is Sorted Or Not
+void Sorted_Positive_Or_Negative(struct Array *arr){
+
+    int i=0, j = arr->length-1;
+
+    while(i < j){
+
+        // Check Number is Positive or Not
+        while(arr->A[i] < 0){
+            i++;
+        }
+
+        // Check Number is Negative Or Not
+        while(arr->A[j] >= 0){
+            j--;
+        }
+
+        // Swap The Number
+        if(i<j){
+            swap(arr->A[i], arr->A[j]);
+        }
+
+    }
+
+}
+
+
+
+int main(){
+    struct Array arr = {{4, 8, 13, 16, 20, 25, 28, 33}, 15, 8};
+    // struct Array arr = {{-4, -8, 13, 16, -20, 25, -28, 33}, 15, 8};
+
+    // Display Array
+    Display(&arr);
+
+    // Call The Function
+    // Sorted_Array(&arr, 18);
+
+    // Check in Positive Number is Sorted Or Not
+    // cout <<Algorithum_is_Sorted(&arr) <<endl;
+
+
+    // Check in Negative Number or Positive number is Sorted Or Not
+    Sorted_Positive_Or_Negative(&arr);
+
+    // Display Array
+    Display(&arr);
+
+
+    return 0;
+}
